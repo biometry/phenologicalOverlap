4fd14243d9075eee02c313def91d4337159710b6
Carsten Dormann <carsten.dormann@biom.uni-freiburg.de>
2017-10-02 15:35:11 +0200

    Initial package skeleton submit; I set up a vignette; the _packaging.R-file contains also the wishlist and the testing

---
+++ phenologicalOverlap_packaging.R
@@ -0,0 +1,22 @@
+library(devtools)
+setwd("~/Data/aktuell/bipartite/phenologicalOverlap")
+document() # process R-functions into .RD files, change namespace
+setwd("..")
+build("phenologicalOverlap", path="phenologicalOverlap")   # build the .tar.gz file
+install("phenologicalOverlap") # install on the computer
+
+
+# write a vignette:
+devtools:::use_vignette("phenologicalOverlap-vignette")
+
+
+# test things:
+
+library(phenologicalOverlap)
+
+
+
+# wishlist
+* real data to play with in the vignette
+* for testthat: analytical solutions and known results cases for verification of code
+* documentation of available metrics in vignette
